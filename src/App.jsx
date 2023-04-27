46db27c98b6c269f191718817436d4a883e4124f
Yehuda-Edelstein <yeauthor@gmail.com>
2023-04-26 20:38:35 -0400

    hope this works

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Dumbnail from "./components/dumbnail/Dumbnail";
 import Header from "./components/header/Header";
@@ -9,7 +9,7 @@ import "./App.scss";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/yehuda">
       <Header />
       <div className="app">
         <Routes>
