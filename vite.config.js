9e30d70880ce3642d6e350f6176dd137caebf166
Yehuda-Edelstein <yeauthor@gmail.com>
2023-05-01 16:44:29 -0400

    404

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    // ...
     rollupOptions: {
-      // ...
       input: {
         main: resolve(__dirname, "index.html"),
         404: resolve(__dirname, "public/404.html"),
